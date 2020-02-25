3f94cddc93230d277dd6cd05f5d3213e710c71ab
Natacha Rivoire <nrivoire@z3r2p4.le-101.fr>
2020-02-25 16:13:45 +0100

    light en cours

--- src/events/key_event.c
+++ src/events/key_event.c
@@ -6,7 +6,7 @@
 /*   By: nrivoire <nrivoire@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/02/10 18:05:31 by nrivoire     #+#   ##    ##    #+#       */
-/*   Updated: 2020/02/24 17:39:22 by nrivoire    ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/02/25 13:10:12 by nrivoire    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -29,23 +29,5 @@ int         key_event(t_env *v, const Uint8 *keyboard_state)
         v->cam.ori.z--;
     if (keyboard_state[SDL_SCANCODE_KP_PLUS])
         v->cam.ori.z++;
-    // contraste lumiere abiante //
-    if (keyboard_state[SDL_SCANCODE_KP_DIVIDE])
-        v->intens += 2;
-    if (keyboard_state[SDL_SCANCODE_KP_MULTIPLY])
-        v->intens -= 2;
-    // lumiere ambiante deplacement //
-    if (keyboard_state[SDL_SCANCODE_I])
-        v->light_ori.y += 1;
-    if (keyboard_state[SDL_SCANCODE_K])
-        v->light_ori.y -= 1;
-    if (keyboard_state[SDL_SCANCODE_L])
-        v->light_ori.x += 1;
-    if (keyboard_state[SDL_SCANCODE_J])
-        v->light_ori.x -= 1;
-    if (keyboard_state[SDL_SCANCODE_U])
-        v->light_ori.z += 1;
-    if (keyboard_state[SDL_SCANCODE_O])
-        v->light_ori.z -= 1;
     return (0);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: nrivoire <nrivoire@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/02/18 18:18:29 by nrivoire     #+#   ##    ##    #+#       */
-/*   Updated: 2020/02/25 12:49:36 by nrivoire    ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/02/25 13:07:50 by nrivoire    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -48,8 +48,6 @@ int				main(int argc, char **argv)
 	v->cam.angle_x = 0;
 	v->cam.angle_y = 0;
 	v->angle_ratio = (v->fov / (float)v->w) * M_PI / 180;
-	v->intens = 15;
-	v->light_ori = (t_vec){0, 10, 0};
 
 	/* -- a changer par les valeurs prises dans le parsing */
 	scene_value(v);
