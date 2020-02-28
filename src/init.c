2a98f461b24262dc38bc7b08c93351fe53de9b99
Valentin Salome <vasalome@z3r1p4.le-101.fr>
2020-02-28 19:31:53 +0100

    final_init _maybe

--- src/init.c
+++ src/init.c
@@ -17,11 +17,9 @@ void			init_value(t_env *v)
 	v->w = 1280;
 	v->h = 720;
 	v->p.sc.amb_light = (t_color){0.5, 0.5, 0.5, 255};
-
 	v->p.cam.pos = (t_vec){0.0, 0.0, 0.0};
 	v->p.cam.dir = (t_vec){0.0, 0.0, 0.0};
 	v->p.cam.fov = 60;
-
 	v->cam.fov = v->p.cam.fov;
 	v->selected_obj = NULL;
 	v->p.ob = NULL;
