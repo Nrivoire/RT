289f254f45d4e7f5f976f0387feaf4f02e79eede
Natacha <natachaNatacha@MacBook-Pro-de-Valentin.local>
2020-05-09 14:35:50 +0200

    plans corriger

--- src/obj_generating/make_tab_obj.c
+++ src/obj_generating/make_tab_obj.c
@@ -6,27 +6,11 @@
 /*   By: nrivoire <nrivoire@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 18:12:17 by nrivoire          #+#    #+#             */
-/*   Updated: 2020/05/07 18:37:58 by nrivoire         ###   ########lyon.fr   */
+/*   Updated: 2020/05/09 14:34:44 by nrivoire         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
-
-void			check_plan(int i, t_env *v)
-{
-	t_vec		ab;
-	t_vec		ac;
-	t_vec		cross;
-
-	ab = vec_sub(v->tab_obj[i].b, v->tab_obj[i].a);
-	ac = vec_sub(v->tab_obj[i].c, v->tab_obj[i].a);
-	cross = vec_cross_product(ab, ac);
-	if (vec_scale_product(ab, ac) != 0)
-	{
-		cross = vec_cross_product(cross, ab);
-		v->tab_obj[i].c = vec_sub(cross, v->tab_obj[i].a);
-	}
-}
 
 void			make_tab_obj(t_env *v)
 {
@@ -39,11 +23,8 @@ void			make_tab_obj(t_env *v)
 			v->tab_obj[i].q = make_sphere(v->tab_obj[i].pos,
 					v->tab_obj[i].radius);
 		else if (v->tab_obj[i].type == PLAN)
-		{
-			check_plan(i, v);
 			v->tab_obj[i].q = make_plan(v->tab_obj[i].a, v->tab_obj[i].b,
 					v->tab_obj[i].c);
-		}
 		else if (v->tab_obj[i].type == CONE)
 			v->tab_obj[i].q = make_cone(v->tab_obj[i].pos, v->tab_obj[i].dir,
 					v->tab_obj[i].radius);
