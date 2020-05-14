54cad24f1e24a1f243e0a40db10a0996871f58a2
qpupier <qpupier@student.42lyon.fr>
2020-05-14 13:13:52 +0200

    modif events

--- src/events/key_event.c
+++ src/events/key_event.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   key_event.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vasalome <vasalome@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: qpupier <qpupier@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 18:05:31 by nrivoire          #+#    #+#             */
-/*   Updated: 2020/05/13 06:51:52 by vasalome         ###   ########lyon.fr   */
+/*   Updated: 2020/05/14 13:11:03 by qpupier          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,34 +27,40 @@ int				cooldown(t_env *v, int seconds)
 
 static void		key_angle(t_env *v, const Uint8 *keyboard_state)
 {
-	if (keyboard_state[SDL_SCANCODE_I] && !keyboard_state[SDL_SCANCODE_SPACE])
-		v->cam.angle_x += 0.02;
-	if (keyboard_state[SDL_SCANCODE_K] && !keyboard_state[SDL_SCANCODE_SPACE])
-		v->cam.angle_x -= 0.02;
-	if (keyboard_state[SDL_SCANCODE_J] && !keyboard_state[SDL_SCANCODE_SPACE])
-		v->cam.angle_y += 0.02;
-	if (keyboard_state[SDL_SCANCODE_L] && !keyboard_state[SDL_SCANCODE_SPACE])
-		v->cam.angle_y -= 0.02;
-	if (keyboard_state[SDL_SCANCODE_O] && !keyboard_state[SDL_SCANCODE_SPACE])
-		v->cam.angle_z -= 0.02;
-	if (keyboard_state[SDL_SCANCODE_U] && !keyboard_state[SDL_SCANCODE_SPACE])
-		v->cam.angle_z += 0.02;
+	if (!keyboard_state[SDL_SCANCODE_SPACE])
+	{
+		if (keyboard_state[SDL_SCANCODE_I])
+			v->cam.angle_x += 0.02;
+		if (keyboard_state[SDL_SCANCODE_K])
+			v->cam.angle_x -= 0.02;
+		if (keyboard_state[SDL_SCANCODE_J])
+			v->cam.angle_y += 0.02;
+		if (keyboard_state[SDL_SCANCODE_L])
+			v->cam.angle_y -= 0.02;
+		if (keyboard_state[SDL_SCANCODE_O])
+			v->cam.angle_z -= 0.02;
+		if (keyboard_state[SDL_SCANCODE_U])
+			v->cam.angle_z += 0.02;
+	}
 }
 
 static void		key_cam(t_env *v, const Uint8 *keyboard_state)
 {
-	if (keyboard_state[SDL_SCANCODE_W] && !keyboard_state[SDL_SCANCODE_SPACE])
-		v->cam.ori.z -= 1.0;
-	if (keyboard_state[SDL_SCANCODE_S] && !keyboard_state[SDL_SCANCODE_SPACE])
-		v->cam.ori.z += 1.0;
-	if (keyboard_state[SDL_SCANCODE_A] && !keyboard_state[SDL_SCANCODE_SPACE])
-		v->cam.ori.x -= 1.0;
-	if (keyboard_state[SDL_SCANCODE_D] && !keyboard_state[SDL_SCANCODE_SPACE])
-		v->cam.ori.x += 1.0;
-	if (keyboard_state[SDL_SCANCODE_E] && !keyboard_state[SDL_SCANCODE_SPACE])
-		v->cam.ori.y += 1.0;
-	if (keyboard_state[SDL_SCANCODE_Q] && !keyboard_state[SDL_SCANCODE_SPACE])
-		v->cam.ori.y -= 1.0;
+	if (!keyboard_state[SDL_SCANCODE_SPACE])
+	{
+		if (keyboard_state[SDL_SCANCODE_W])
+			v->cam.ori.z -= 1.0;
+		if (keyboard_state[SDL_SCANCODE_S])
+			v->cam.ori.z += 1.0;
+		if (keyboard_state[SDL_SCANCODE_A])
+			v->cam.ori.x -= 1.0;
+		if (keyboard_state[SDL_SCANCODE_D])
+			v->cam.ori.x += 1.0;
+		if (keyboard_state[SDL_SCANCODE_E])
+			v->cam.ori.y += 1.0;
+		if (keyboard_state[SDL_SCANCODE_Q])
+			v->cam.ori.y -= 1.0;
+	}
 }
 
 void			key_post_process(t_env *v, const Uint8 *keyboard_state)
