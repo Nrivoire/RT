09d28fa7eb21a99db3417f99bedc3400bf718720
vasalome <vasalome>
2020-05-16 20:43:16 +0200

    for_merge

--- src/obj_generating/make_sphere.c
+++ src/obj_generating/make_sphere.c
@@ -6,29 +6,11 @@
 /*   By: vasalome <vasalome@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/07 14:19:32 by natachaNata       #+#    #+#             */
-/*   Updated: 2020/05/15 16:03:38 by vasalome         ###   ########lyon.fr   */
+/*   Updated: 2020/05/16 17:41:52 by vasalome         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
-
-t_quadric		make_sphere(t_vec center, float radius)
-{
-	t_quadric	res;
-
-	res.a = 1;
-	res.b = 1;
-	res.c = 1;
-	res.d = 0;
-	res.e = 0;
-	res.f = 0;
-	res.g = -2 * center.x;
-	res.h = -2 * center.y;
-	res.i = -2 * center.z;
-	res.j = center.x * center.x + center.y * center.y + center.z * center.z
-			- radius * radius;
-	return (res);
-}
 
 // t_quadric		make_sphere(t_vec center, float radius)
 // {
@@ -43,7 +25,25 @@ t_quadric		make_sphere(t_vec center, float radius)
 // 	res.g = -2 * center.x;
 // 	res.h = -2 * center.y;
 // 	res.i = -2 * center.z;
-// 	res.j = (center.x * center.x + center.y * center.y + center.z * center.z
-// 			- radius * radius ) / 1;
+// 	res.j = center.x * center.x + center.y * center.y + center.z * center.z
+// 			- radius * radius;
 // 	return (res);
 // }
+
+t_quadric		make_sphere(t_vec center, float radius)
+{
+	t_quadric	res;
+
+	res.a = 1;
+	res.b = 1;
+	res.c = 1;
+	res.d = 0;
+	res.e = 0;
+	res.f = 0;
+	res.g = -2 * center.x;
+	res.h = -2 * center.y;
+	res.i = -2 * center.z;
+	res.j = (center.x * center.x + center.y * center.y + center.z * center.z
+			- radius * radius ) / 1;
+	return (res);
+}
