e3d0bc0588c71446ad407568980812faa3a8a3fa
vasalome <vasalome>
2020-05-17 19:16:48 +0200

    limit_define_celshading

--- src/post_process/cel_shading.c
+++ src/post_process/cel_shading.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cel_shading.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nrivoire <nrivoire@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: vasalome <vasalome@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/05 19:33:50 by vasalome          #+#    #+#             */
-/*   Updated: 2020/05/09 22:01:54 by nrivoire         ###   ########lyon.fr   */
+/*   Updated: 2020/05/17 18:24:23 by vasalome         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,11 @@ int		cel_shading(int color)
 	r = color >> 24 & 0xFF;
 	g = color >> 16 & 0xFF;
 	b = color >> 8 & 0xFF;
-	r = (int)((r / CELSHADING) * CELSHADING << 24);
-	g = (int)((g / CELSHADING) * CELSHADING << 16);
-	b = (int)((b / CELSHADING) * CELSHADING << 8);
+	r = (int)((r / ft_clamp(CELSHADING, 2, 60))\
+		* ft_clamp(CELSHADING, 2, 60) << 24);
+	g = (int)((g / ft_clamp(CELSHADING, 2, 60))\
+		* ft_clamp(CELSHADING, 2, 60) << 16);
+	b = (int)((b / ft_clamp(CELSHADING, 2, 60))\
+		* ft_clamp(CELSHADING, 2, 60) << 8);
 	return (r | g | b | (color & 0xFF));
 }
