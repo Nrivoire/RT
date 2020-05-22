a1b69ec8559ac1bf7ef185ba7355522b269aee34
vasalome <vasalome>
2020-05-22 17:05:47 +0200

    goodnight_obj

--- src/events/obj_event.c
+++ src/events/obj_event.c
@@ -6,11 +6,30 @@
 /*   By: vasalome <vasalome@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 18:05:31 by nrivoire          #+#    #+#             */
-/*   Updated: 2020/05/21 05:13:11 by vasalome         ###   ########lyon.fr   */
+/*   Updated: 2020/05/22 15:40:38 by vasalome         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
+
+static void	goodnight_obj(t_env *v)
+{
+	v->selected_obj->radius = 0;
+	v->selected_obj->pos = (t_vec){15000, 15000, 15000};
+	v->selected_obj->dir = (t_vec){0,0,1};
+	v->selected_obj->a = (t_vec){15000, 15000, 15000};
+	v->selected_obj->b = (t_vec){15000, 15000, 15000};
+	v->selected_obj->c = (t_vec){15000, 15000, 15000};
+	v->selected_obj->color = (t_color){0,0,0};
+	v->selected_obj->reflect = 0.0;
+	v->selected_obj->refract = 0.0;
+	v->selected_obj->rho = 0.0;
+	v->selected_obj->shininess = 0.0;
+	v->selected_obj->procedural = 0;
+	v->selected_obj->texture = NULL;
+	v->selected_obj->q = (t_quadric){0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	v->selected_obj = NULL;
+}
 
 static void	obj_event_rotate(t_env *v, const Uint8 *keyboard_state)
 {
@@ -55,4 +74,6 @@ void		obj_event(t_env *v, const Uint8 *keyboard, float scale)
 		plane_event(v, keyboard, scale);
 		plane_rotate(v, keyboard);
 	}
+	if (keyboard[SDL_SCANCODE_G] && keyboard[SDL_SCANCODE_N])
+		goodnight_obj(v);
 }
